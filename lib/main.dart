21eec400e1e836b001220482191958f0a0513e07
unknown <raffaykhan29@gmail.com>
2025-05-17 01:56:27 +0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Flutter Demo',
+      home: Material(child: Center(child: Text("MY NAME IS RAFFAY"))),
+    );
+  }
+}
